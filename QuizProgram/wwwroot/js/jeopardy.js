bdfeedb9b43286ec93284fef7c7f2461fb5c5232
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-04-27 20:06:42 +0200

    Made updates to QuizProgram - jeopardy

---
+++ QuizProgram/wwwroot/js/jeopardy.js
@@ -0,0 +1,10 @@
+﻿function playVideo() {
+    console.log("video called");
+    var video = document.getElementById("bookVideo");
+    video.play();
+}
+
+function fadeInText() {
+    var text = document.getElementById("overlay-text");
+    text.classList.add("fade-in-text");
+}
